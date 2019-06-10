384713a8d3ce6d004a31e1ac8d80c110d2ee6404
Patrick Quinn-Graham <make-contact@pftqg.com>
2019-06-10 10:08:33 +1000

    Initial db schema

--- src/tools/initdb.ts
+++ src/tools/initdb.ts
@@ -19,7 +19,36 @@ const sql: string[] = [
       group_id varchar(50) NOT NULL,
       brand varchar(255) NOT NULL
     )
-    `,
+  `,
+  `
+    CREATE TABLE IF NOT EXISTS group_users (
+      group_id INT NOT NULL,
+      user_id varchar(255) NOT NULL
+    )
+  `,
+  `
+    CREATE TABLE IF NOT EXISTS events (
+      event_id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+      group_id INT NOT NULL,
+      name varchar(255) NOT NULL,
+      start_time timestamp
+    )
+  `,
+  `
+    CREATE TABLE IF NOT EXISTS talks (
+      talk_id INT NOT NULL,
+      event_id INT NOT NULL,
+      title varchar(255) NOT NULL,
+      fps INT NOT NULL
+    )
+  `,
+  `
+    CREATE TABLE IF NOT EXISTS talk_presenters (
+      talk_id INT NOT NULL,
+      user_id varchar(255) NOT NULL,
+      order INT NOT NULL
+    )
+  `,
 ];
 
 asyncMain(async () => {
